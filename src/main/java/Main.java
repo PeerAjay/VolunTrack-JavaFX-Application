c90393ba12d6f991bc35d47e05c7dc4245d73ce9
AjayPeeris <s4088958@student.rmit.edu.au>
2025-09-24 12:50:53 +1000

    Intitializing the project

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}

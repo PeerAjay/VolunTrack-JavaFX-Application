8d7dee2670b7c111f86e699fb3cea2beee219652
AjayPeeris <s4088958@student.rmit.edu.au>
2025-10-01 10:50:46 +1000

    Added fields and contructor to Project class

--- src/main/java/model/Project.java
+++ src/main/java/model/Project.java
@@ -1,4 +1,26 @@
 package model;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.IntegerProperty;
+
+import javax.swing.*;
 
 public class Project {
+    private final StringProperty title;
+    private final StringProperty location;
+    private final StringProperty day;
+    private final IntegerProperty hourlyValue;
+    private final IntegerProperty regSlots;
+    private final IntegerProperty totalSlots;
+
+    public Project(StringProperty title, StringProperty location, StringProperty day, IntegerProperty hourlyValue,
+                   IntegerProperty regSlots, IntegerProperty totalSlots) {
+        this.title = title;
+        this.location = location;
+        this.day = day;
+        this.hourlyValue = hourlyValue;
+        this.regSlots = regSlots;
+        this.totalSlots = totalSlots;
+    }
 }
+

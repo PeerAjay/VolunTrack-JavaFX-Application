48531a0d1c8d6ca12713d32770a90177f794b735
AjayPeeris <s4088958@student.rmit.edu.au>
2025-10-16 11:16:07 +1100

    Added controller for add project popup

---
+++ src/main/java/controller/addProjectPopupController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.awt.*;
+import java.sql.SQLException;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import model.Model;
+import model.User;
+import util.AuthenticationManager;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class addProjectPopupController {
+    @FXML
+    private TextField numSlotsInput; //Corresponds to the number of slots field
+    @FXML
+    private TextField numHoursInput; //Corresponds to the number of hours field
+    @FXML
+    private Button addInputToCart; //Corresponds to add to cart button
+    @FXML
+    private Button cancel; //Corresponds to the cancel button
+
+    private Stage stage;
+    private Stage parentStage;
+    private Model model;
+
+    public addProjectPopupController(){
+        this.stage = new Stage();
+        this.parentStage = parentStage;
+        this.model = model;
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+
+}

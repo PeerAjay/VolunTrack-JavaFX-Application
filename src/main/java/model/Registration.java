8219058d8cee0e5191fc85a759c63350c6b10cac
AjayPeeris <s4088958@student.rmit.edu.au>
2025-10-17 20:19:10 +1100

    Created registration object model

---
+++ src/main/java/model/Registration.java
@@ -0,0 +1,72 @@
+package model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.time.LocalDateTime;
+
+public class Registration {
+    private final IntegerProperty registrationID = new SimpleIntegerProperty();
+    private final StringProperty userId = new SimpleStringProperty();
+    private final IntegerProperty projectId = new SimpleIntegerProperty();
+    private final IntegerProperty regSlots = new SimpleIntegerProperty();
+    private final IntegerProperty hoursPerSlot = new SimpleIntegerProperty();
+    private final IntegerProperty totalContribution = new SimpleIntegerProperty();
+    private final LocalDateTime timeStamp;
+
+    public Registration(int registrationID, String userId, int projectId, int regSlots, int hoursPerSlot, int totalContribution, LocalDateTime timeStamp){
+        setRegistrationID(registrationID);
+        setUserId(userId);
+        setProjectId(projectId);
+        setRegSlots(regSlots);
+        setHoursPerSlot(hoursPerSlot);
+        setTotalContribution(totalContribution);
+        this.timeStamp = timeStamp;
+    };
+
+    public int getRegistrationID(){
+        return registrationID.get();
+    }
+    public void setRegistrationID(int value){
+        registrationID.set(value);
+    }
+
+    public String getUserId(){
+        return userId.get();
+    }
+    public void setUserId(String value){
+        userId.set(value);
+    }
+
+    public int getProjectId(){
+        return projectId.get();
+    }
+    public void setProjectId(int value){
+        projectId.set(value);
+    }
+
+    public int getRegSlots(){
+        return regSlots.get();
+    }
+    public void setRegSlots(int value){
+        regSlots.set(value);
+    }
+
+    public int getHoursPerSlot(){
+        return hoursPerSlot.get();
+    }
+    public void setHoursPerSlot(int value){
+        hoursPerSlot.set(value);
+    }
+
+    public int getTotalContribution(){
+        return totalContribution.get();
+    }
+    public void setTotalContribution(int value){
+        totalContribution.set(value);
+    }
+
+
+}
